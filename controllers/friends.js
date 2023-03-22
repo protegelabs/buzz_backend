fb465885b40a905b33ba7f4422f9b3278b7c44b5
raji2004 <kingraj1344@gmail.com>
2023-03-22 23:24:29 +0100

    done with friend feature

--- controllers/friends.js
+++ controllers/friends.js
@@ -11,13 +11,37 @@ const uniqid = require('uniqid');
 
 exports.friendRequest = async (req, res) => {
     const { sender, receiver, senderName } = req.body;
-    const id = uniqid();
-    const newFriendRequest = await Friend.create({ id, friend_id: sender, user_id: receiver, friendName: senderName });
-    res.send(newFriendRequest);
+    try {
+        const id = uniqid();
+        const newFriendRequest = await Friend.create({ id, friend_id: sender, user_id: receiver, friendName: senderName });
+        res.status(200).json({ ...newFriendRequest.dataValues });
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
 }
 
 exports.getFriends = async (req, res) => {
     const { id } = req.body;
-    const friends = await Friend.findAll({ where: { [Op.or]: [{ user_id: id }, { friend_id: id }] } });
-    res.send(friends);
+    try {
+        const friends = await Friend.findAll({ where: { [Op.or]: [{ user_id: id }, { friend_id: id }] } });
+        res.status(200).send(friends);
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
 }
+
+exports.changeFriendStatus = async (req, res) => {
+    const { sender, status, receiver } = req.body;
+    try {
+        const friend = await Friend.findOne({ where: { [Op.and]: [{ user_id: receiver }, { friend_id: sender }] } });
+        friend.status = status;
+        await friend.save();
+        res.status(200).send(friend);
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+   
+}
+

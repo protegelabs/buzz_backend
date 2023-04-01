547c9d59c3e65ce24ad0dcc31841284f57df6cc5
raji2004 <kingraj1344@gmail.com>
2023-04-01 12:46:21 +0100

    rr

--- models/models.js
+++ models/models.js
@@ -3,7 +3,8 @@ const { DataTypes, FLOAT } = require('sequelize');
 const { sequelize } = require('../config/sequelize')
 const { STRING, INTEGER, DATE, BOOLEAN } = DataTypes
 
-// Define models for each table
+// Define models for each table 
+// pushing to dev
 
 //1. USER SCHEMA
 const User = sequelize.define('User', {

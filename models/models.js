92d01f8cd2ae64bc5bf93e481f3bf00b23525909
raji2004 <kingraj1344@gmail.com>
2023-04-30 10:39:03 +0100

    done with nearest location

--- models/models.js
+++ models/models.js
@@ -144,6 +144,23 @@ const Event = sequelize.define('Event', {
     modelName: 'events'
 
 });
+  Event.addScope('distance', (latitude, longitude, distance, unit = "km") => {
+    const constant = unit == "km" ? 6371 : 3959;
+    const haversine = `(
+        ${constant} * acos(
+            cos(radians(${latitude}))
+            * cos(radians(latitude))
+            * cos(radians(longitude) - radians(${longitude}))
+            + sin(radians(${latitude})) * sin(radians(latitude))
+        )
+    )`;
+    return {
+        attributes: [ 
+            [sequelize.literal(haversine), 'distance'],
+        ],
+        having: sequelize.literal(`distance <= ${distance}`)
+    }
+}) 
 
 
 //3. POST SCHEMA

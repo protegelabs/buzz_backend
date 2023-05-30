f73856faffd030768f019a39a8f910f270a9de3d
samuelikoli <ayibanimiikoli@gmail.com>
2023-05-30 15:28:56 +0100

    get routes to post

--- controllers/Follow.js
+++ controllers/Follow.js
@@ -3,7 +3,7 @@ const { Follow } = require('../models/models');
 exports.createFollow = async (req, res) => {
     const { host, user_id } = req.body
     try {
-        const fav = await Follow.create({ id: uniqid(), host, follower:user_id })
+        const fav = await Follow.create({ id: uniqid(), host, follower: user_id })
         res.status(200).json({ fav })
     } catch (err) {
         res.status(400).json({ message: err.message })
@@ -13,28 +13,28 @@ exports.createFollow = async (req, res) => {
 exports.getFollows = async (req, res) => {
     const { user_id } = req.body
     try {
-        const fav = await Follow.findAll({ where: { follower:user_id } })
+        const fav = await Follow.findAll({ where: { follower: user_id } })
         res.status(200).json({ fav })
     } catch (err) {
         res.status(400).json({ message: err.message })
     }
 }
-exports.getHostFollow= async(req,res)=>{
-     const{host }= req.body
-     try{
+exports.getHostFollow = async (req, res) => {
+    const { host } = req.body
+    try {
         const fav = await Follow.findAll({ where: { host } })
-        res.status(200).json({ fav,followcount:fav.length })
-     }catch(err){
-         res.status(400).json({message:err.message})
-     }
+        res.status(200).json({ fav, followcount: fav.length })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 }
 
-exports.unFollow= async(req,res)=>{
-     const{user_id,host }= req.body
-     try{
-        const fav = await Follow.destroy({ where: { host,follower:user_id } })
-        res.status(200).json({ fav})
-     }catch(err){
-         res.status(400).json({message:err.message})
-     }
+exports.unFollow = async (req, res) => {
+    const { user_id, host } = req.body
+    try {
+        const fav = await Follow.destroy({ where: { host, follower: user_id } })
+        res.status(200).json({ fav })
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 }
